d976309110fb8cd9f561d2f63e40c6da1a83ffb5
Christopher Tokar <christopher.tokar@gmail.com>
2013-12-20 20:45:01 +0100

    fixed calculations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,3 +12,18 @@ var epssum = parseFloat($('#eps1').html()) +
     parseFloat($('#eps9').html());
 
 $('#epssum').html(epssum.toFixed(2));
+
+var divsum = parseFloat($('#div1').html()) +
+    parseFloat($('#div2').html()) +
+    parseFloat($('#div3').html()) +
+    parseFloat($('#div4').html()) +
+    parseFloat($('#div5').html()) +
+    parseFloat($('#div6').html()) +
+    parseFloat($('#div7').html()) +
+    parseFloat($('#div8').html()) +
+    parseFloat($('#div9').html());
+
+$('#divsum').html(divsum);
+
+var totalcash = bookvaluediff + divsum
+$('#totalcash').html(totalcash);
