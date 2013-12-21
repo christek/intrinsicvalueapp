e210e5368efd8e95b2de573796a7fb312c1ea09e
Christopher Tokar <christopher.tokar@gmail.com>
2013-12-21 20:42:37 +0100

    book value calculation fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,3 @@
-var bookvaluediff = parseFloat($('#bookvalue2').html()) - parseFloat($('#bookvalue1').html());
-$('#bookvaluediff').html(bookvaluediff);
 
 var epssum = parseFloat($('#eps1').html()) +
     parseFloat($('#eps2').html()) +
@@ -27,3 +25,20 @@ $('#divsum').html(divsum);
 
 var totalcash = bookvaluediff + divsum
 $('#totalcash').html(totalcash);
+
+
+var cbv = parseFloat($('#bookvalue2').html());
+console.log(cbv)
+var obv = parseFloat($('#bookvalue1').html());
+console.log(obv)
+var years = 9;
+
+var upper = 1 / years;
+var base = cbv / obv;
+var a = Math.pow(base, upper);
+var bookvaluediff = 100 * (a - 1);
+
+$('#bookvaluediff').html(bookvaluediff.toFixed(3));
+
+
+
