dd0cf607983b2a7851e3eb077040d3a6580939d4
Christopher Tokar <christopher.tokar@gmail.com>
2013-12-28 14:09:02 +0100

    added line graph

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,33 +1,33 @@
 
-var epssum = parseFloat($('#eps1').html()) +
-    parseFloat($('#eps2').html()) +
-    parseFloat($('#eps3').html()) +
-    parseFloat($('#eps4').html()) +
-    parseFloat($('#eps5').html()) +
-    parseFloat($('#eps6').html()) +
-    parseFloat($('#eps7').html()) +
-    parseFloat($('#eps8').html()) +
-    parseFloat($('#eps9').html());
+var epssum = parseFloat($('#eps1').val()) +
+    parseFloat($('#eps2').val()) +
+    parseFloat($('#eps3').val()) +
+    parseFloat($('#eps4').val()) +
+    parseFloat($('#eps5').val()) +
+    parseFloat($('#eps6').val()) +
+    parseFloat($('#eps7').val()) +
+    parseFloat($('#eps8').val()) +
+    parseFloat($('#eps9').val());
 
 $('#epssum').html(epssum.toFixed(2));
 
-var divsum = parseFloat($('#div1').html()) +
-    parseFloat($('#div2').html()) +
-    parseFloat($('#div3').html()) +
-    parseFloat($('#div4').html()) +
-    parseFloat($('#div5').html()) +
-    parseFloat($('#div6').html()) +
-    parseFloat($('#div7').html()) +
-    parseFloat($('#div8').html()) +
-    parseFloat($('#div9').html());
+var divsum = parseFloat($('#div1').val()) +
+    parseFloat($('#div2').val()) +
+    parseFloat($('#div3').val()) +
+    parseFloat($('#div4').val()) +
+    parseFloat($('#div5').val()) +
+    parseFloat($('#div6').val()) +
+    parseFloat($('#div7').val()) +
+    parseFloat($('#div8').val()) +
+    parseFloat($('#div9').val());
 
 $('#divsum').html(divsum);
 
 
 
 
-var cbv = parseFloat($('#bookvalue2').html());
-var obv = parseFloat($('#bookvalue1').html());
+var cbv = parseFloat($('#bookvalue2').val());
+var obv = parseFloat($('#bookvalue1').val());
 var years = 9;
 
 var upper = 1 / years;
@@ -49,15 +49,27 @@ var bookvaluedifferencefloat = bookvaluedifference
 $('#bookvaluediff').html(bookvaluedifferencefloat);
 
 
-var dividendsForOneYear = parseFloat($('#div9').html());
+var total = bookvaluedifferencefloat + divsum;
+$("#totalcashsum").html(total);
+
+
+//**************************************************
+var dividendsForOneYear = parseFloat($('#div9').val());
 $('#totalcash').html(dividendsForOneYear);
+
+
+$("#currentBookValue").html(cbv);
+
+
+
+
 
 var year=10;
 var r=1.71; // update this
 
 $("#currentnote").html(r);
 
-var bvc=bookvaluedifference;
+var bvc=bookvaluepercentagechange;
 
 var perc=(1+bvc/100);
 
@@ -73,45 +85,4 @@ extra=Math.pow((1+r),year);
 var c = dividendsForOneYear*(1-(1/extra))/r+parr/extra;
 
 $('#intrinsicValue').html(c.toFixed(2));
-
-
-
-var total = bookvaluedifferencefloat + divsum;
-$("#totalcashsum").html(total);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-coupon=Number(document.calculator.coupon.value);
-par=Number(document.calculator.par.value);
-year=Number(document.calculator.year.value);
-r=Number(document.calculator.r.value);
-bvc=Number(document.calculator.bvc.value);
-
-perc=(1+bvc/100);
-base=Math.pow(perc,year);
-parr=par*base;
-r=r/100;
-
-extra=Math.pow((1+r),year);
-
-c=coupon*(1-(1/extra))/r+parr/extra;
-
-document.calculator.total.value=c;
-*/
-
-
-
-
 
